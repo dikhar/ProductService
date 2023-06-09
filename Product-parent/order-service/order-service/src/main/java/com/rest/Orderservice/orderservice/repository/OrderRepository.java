533f32037f73d262858efab906000d8edd4a9bff
Hardik Sharma <128139937+dikhar@users.noreply.github.com>
2023-06-09 23:19:28 +0530

    Delete Product-parent/order-service/order-service directory

--- Product-parent/order-service/order-service/src/main/java/com/rest/Orderservice/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rest.Orderservice.orderservice.repository;
-
-import com.rest.Orderservice.orderservice.model.order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<order,Long> {
-}
